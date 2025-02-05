96b90465abb09dc1b7b3128fecd5965c964d070e
Руслан <ruslanzhuminov@mail.ru>
2025-02-05 12:18:37 +0300

    first lesson

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -0,0 +1,36 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.jupiter.Spending;
+import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@ExtendWith(BrowserExtension.class)
+public class SpendingWebTest {
+    private static final Config CFG = Config.getInstance();
+
+    @Spending(
+            username = "rus",
+            category = "Обучение",
+            description = "Обучение Advanced 2.0",
+            amount = 79990
+    )
+    @Test
+    void categoryDescriptionShouldBeChangedFromTable(SpendJson spend) {
+        final String newDescription = "Обучение Niffler Next Generation";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("rus", "rus")
+                .editSpending(spend.description())
+                .setNewSpendingDescription(newDescription)
+                .save();
+
+        new MainPage().checkThatTableContainsSpending(newDescription);
+    }
+}
